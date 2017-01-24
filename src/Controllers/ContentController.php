5f6ecf0c7fb6e3191552fc292226ec7971e0b51a
Vytautas <vytautas.sakalauskas@hashtages.com>
2017-01-24 10:24:18 +0000

    Try Parse Array 3

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -36,10 +36,11 @@ class ContentController extends Controller
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $url);
         curl_setopt($curl, CURLOPT_HTTPGET, true);
-        
+        $result = [];
+        $result['time']= $resp;
         $resp = curl_exec($curl);
         curl_close($curl);
-        return $twig->render('ChangePricesPlugin::content.hello',$resp);
+        return $twig->render('ChangePricesPlugin::content.hello',$result);
 
       /*  $response = file_get_contents ( '' );
         if($response) 
